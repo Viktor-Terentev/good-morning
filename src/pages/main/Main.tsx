7f0950adba6e816263ef31f8b100d8c22ddf2276
Viktor <viktor1211200411@gmail.com>
2024-06-03 01:39:55 +0300

    Add for text

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,14 +1,19 @@
 import cl from "./Main.module.css";
 import me from "../../assets/me.jpg"
 import sun from "../../assets/sun.webp"
-import Touch from "./Touch";
+import {useState} from "react";
 
 const Main = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(true);
+    };
     return (
         <div className={cl.main_container}>
             <div className={cl.main_img}>
                 <img src={me} alt="me" className={cl.me}/>
-                <img src={sun} alt="sun" className={cl.sun}/>
+                <img src={sun} alt="sun" className={cl.sun} onClick={handleClick}/>
             </div>
             <div className={cl.main_text}>
                 <h1>Я еще сплю, но желаю тебе доброго утречка</h1>
@@ -16,7 +21,9 @@ const Main = () => {
                 <h1>И приятного массажа</h1>
                 <h1>И отличного настроения</h1>
             </div>
-            <Touch/>
+            <div className={cl.main_touch}>
+                {isClicked ? <h1>Ты самая лучшая, солнце!</h1> : <h1>Нажми на Солнце</h1>}
+            </div>
         </div>
     );
 };
