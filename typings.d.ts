861f863dd01ba29dccd84603e3f7c760aefa0092
Viktor <viktor1211200411@gmail.com>
2024-06-03 01:27:49 +0300

    Add typings

--- typings.d.ts
+++ typings.d.ts
@@ -2,3 +2,13 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
